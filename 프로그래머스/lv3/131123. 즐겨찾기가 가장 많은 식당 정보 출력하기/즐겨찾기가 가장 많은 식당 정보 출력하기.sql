b96ceb52af787c94aa419bb7caf848726c4c9a98
hw03822 <83943948+hw03822@users.noreply.github.com>
2023-03-28 00:02:39 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -2,11 +2,23 @@
 -- select distinct food_type, max(favorites) over(partition by food_type)
 -- from REST_INFO 
 
-select i1.food_type, i1.rest_id, i1.rest_name, i1.favorites
-from REST_INFO i1, 
+-- 답1
+-- select i1.food_type, i1.rest_id, i1.rest_name, i1.favorites
+-- from REST_INFO i1, 
+--     (
+--         select distinct food_type, max(favorites) over(partition by food_type) as favorites
+--         from REST_INFO 
+--     ) i2
+-- where i1.food_type = i2.food_type and i1.favorites = i2.favorites
+-- order by food_type desc
+
+-- 답2
+select food_type, rest_id, rest_name, favorites
+from  
     (
-        select distinct food_type, max(favorites) over(partition by food_type) as favorites
+        select distinct food_type, rest_id, rest_name, favorites,
+                ROW_NUMBER() over(partition by food_type order by favorites desc) as rownumber
         from REST_INFO 
-    ) i2
-where i1.food_type = i2.food_type and i1.favorites = i2.favorites
+    ) i
+where rownumber = 1
 order by food_type desc

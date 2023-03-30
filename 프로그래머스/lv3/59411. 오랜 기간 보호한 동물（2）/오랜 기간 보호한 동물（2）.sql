5dc12ea477f347b0bdba7c71be520e59d927f45b
hw03822 <83943948+hw03822@users.noreply.github.com>
2023-03-31 00:14:15 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -5,11 +5,8 @@
 -- order by period desc
 
 -- 답
-select animal_id, name
-from (
-        SELECT i.animal_id, i.name, i.datetime, o.datetime, 
-        o.datetime - i.datetime as period
-        from animal_ins i inner join animal_outs o on i.animal_id = o.animal_id
-        order by period desc
-     )
-where rownum <= 2
+SELECT i.animal_id, i.name
+from animal_ins i, animal_outs o 
+where i.animal_id = o.animal_id
+order by o.datetime - i.datetime desc
+fetch next 2 rows only -- 두개 봅기 

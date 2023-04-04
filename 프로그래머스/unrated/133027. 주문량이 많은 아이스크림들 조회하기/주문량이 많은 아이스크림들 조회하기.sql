4382370ac4e46603cd93ce7cbf320f3f32e53604
hw03822 <83943948+hw03822@users.noreply.github.com>
2023-04-05 01:02:08 +0900

    [unrated] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,17 +1,34 @@
 -- 7월 아이스크림 총 주문량 + 상반기 아이스크림 총 주문량 더한 값 부터 줄세우고 상위 3개 조회하가
--- 맛이 기준이 되야해
+-- 맛이 기준이 되어야해
 -- SELECT  h.shipment_id, h.flavor, sum(j.total_order) as july_total
 -- FROM FIRST_HALF h, JULY j
 -- WHERE h.flavor = j.flavor
 -- group by h.shipment_id, h.flavor
 
-select h.flavor--, july_total + h.total_order as total
-from (
-    SELECT  h.shipment_id, h.flavor, sum(j.total_order) as july_total
-    FROM FIRST_HALF h, JULY j
-    WHERE h.flavor = j.flavor
-    group by h.shipment_id, h.flavor    
-    ) hj, first_half h
-where hj.flavor = h.flavor
-order by july_total + h.total_order desc
+--답1
+-- select h.flavor -- 상반기 판매량 더하기
+-- from ( -- 7월 아이스크림 판매량 더한후 
+--     SELECT  h.shipment_id, h.flavor, sum(j.total_order) as july_total
+--     FROM FIRST_HALF h, JULY j
+--     WHERE h.flavor = j.flavor
+--     group by h.shipment_id, h.flavor    
+--     ) hj, first_half h
+-- where hj.flavor = h.flavor
+-- order by july_total + h.total_order desc
+-- fetch first 3 rows only
+
+--답2
+SELECT h.flavor
+FROM ( -- 상반기 아이스크림 판매량
+        SELECT flavor, sum(total_order) as half_total 
+        from FIRST_HALF 
+        group by flavor
+    ) h, 
+    ( -- 7월 아이스크림 판매량
+        SELECT flavor, sum(total_order) as july_total
+        from JULY  
+        group by flavor
+    ) j
+where h.flavor = j.flavor
+order by half_total + july_total desc
 fetch first 3 rows only
